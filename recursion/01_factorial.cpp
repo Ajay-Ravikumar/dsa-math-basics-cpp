6475f9c2a6e5091ba92a76911fc41665120d165e
Ajay-Ravikumar <ajay.ravikumar@yahoo.com>
2025-09-20 12:20:50 +0200

    factorial_recursion_cpp_code

---
+++ recursion/01_factorial.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int factorial(int n) {
+    if (n <= 1) return 1;  // base case
+    return n * factorial(n - 1);
+}
+
+int main() {
+    cout << factorial(5) << endl; // 120
+}
