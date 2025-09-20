2b7c8b28f4cd3fdd92657ee82aa9bd7dc41e6142
Ajay-Ravikumar <ajay.ravikumar@yahoo.com>
2025-09-20 14:19:11 +0200

    Create 11_permutations.c

---
+++ recursion/C_Language/11_permutations.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+void swap(char *a, char *b){ char t=*a; *a=*b; *b=t; }
+void permute(char *s, int l, int r){
+if(l == r){
+printf("%s\n", s);
+return;
+}
+for(int i=l;i<=r;i++){
+swap(&s[l], &s[i]);
+permute(s, l+1, r);
+swap(&s[l], &s[i]);
+}
+}
+
+int main(){
+char s[1000]; scanf("%s", s);
+permute(s, 0, strlen(s)-1);
+return 0;
+}
