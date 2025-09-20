7c8c316c39976d67448fa527982ce5e1183413ba
Ajay-Ravikumar <ajay.ravikumar@yahoo.com>
2025-09-20 14:02:31 +0200

    Create 07_check_palindrome.cpp

---
+++ recursion/07_check_palindrome.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isPal(const string &s, int l, int r){
+if(l >= r) return true;
+if(s[l] != s[r]) return false;
+return isPal(s, l+1, r-1);
+}
+int main(){
+string s; cin >> s;
+cout << (isPal(s, 0, s.size()-1) ? "YES" : "NO") << "\n";
+}
